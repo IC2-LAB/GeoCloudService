948432705d5fb48a33b8b30537833eba41095167
Yufan <2435184615@qq.com>
2025-04-18 17:33:38 +0800

    修改了CacheManager实现，增加对并发的支持

--- src/utils/CacheManager.py
+++ src/utils/CacheManager.py
@@ -1,73 +1,83 @@
-import time
-from threading import Lock
+from cachetools import TTLCache
+import threading
+import hashlib
+
+
+class ReadWriteLock:
+    def __init__(self):
+        self._lock = threading.Lock()
+        self._read_ready = threading.Condition(self._lock)
+        self._readers = 0
+
+    def acquire_read(self):
+        with self._lock:
+            self._readers += 1
+            self._read_ready.notify()
+
+    def release_read(self):
+        with self._lock:
+            self._readers -= 1
+            self._read_ready.notify()
+
+    def acquire_write(self):
+        with self._lock:
+            while self._readers > 0:
+                self._read_ready.wait()
+
+    def release_write(self):
+        with self._lock:
+            self._read_ready.notify()
 
 class SimpleCache:
-    def __init__(self, ttl: int = 300):
-        """
-        初始化缓存工具类
-        :param ttl: 缓存过期时间（秒）
-        """
-        self.cache = {}
-        self.ttl = ttl
-        self.lock = Lock()
+    def __init__(self, maxsize: int = 128, ttl: int = 300):
+        self.cache = TTLCache(maxsize=maxsize, ttl=ttl)
+        self.rw_lock = ReadWriteLock()  # 使用读写锁（如上定义）
 
     def set(self, key: str, value: any):
-        """
-        设置缓存
-        :param key: 缓存键
-        :param value: 缓存值
-        """
-        with self.lock:
-            self.cache[key] = (time.time(), value)
+        self.rw_lock.acquire_write()
+        try:
+            self.cache[key] = value
+        finally:
+            self.rw_lock.release_write()
 
     def get(self, key: str):
-        """
-        获取缓存
-        :param key: 缓存键
-        :return: 缓存值,如果缓存不存在或过期则返回None
-        """
-        with self.lock:
-            if key in self.cache:
-                timestamp, value = self.cache[key]
-                if time.time() - timestamp < self.ttl:
-                    return value
-                else:
-                    # 缓存过期，删除缓存
-                    del self.cache[key]
-            return None
+        self.rw_lock.acquire_read()
+        try:
+            return self.cache.get(key)
+        finally:
+            self.rw_lock.release_read()
 
     def delete(self, key: str):
-        """
-        删除缓存
-        :param key: 缓存键
-        """
-        with self.lock:
-            if key in self.cache:
-                del self.cache[key]
+        self.rw_lock.acquire_write()
+        try:
+            self.cache.pop(key, None)
+        finally:
+            self.rw_lock.release_write()
 
     def clear(self):
-        """
-        清空缓存
-        """
-        with self.lock:
+        self.rw_lock.acquire_write()
+        try:
             self.cache.clear()
+        finally:
+            self.rw_lock.release_write()
 
 class CacheManager:
     def __init__(self, cache: SimpleCache):
         self.cache = cache
 
-    def getCacheKey(self, func_name: str, *args, **kwargs):
-        """生成缓存键"""
-        # 使用函数名和参数生成缓存键
-        key_parts = [func_name] + list(args) + [f"{k}={v}" for k, v in kwargs.items()]
-        return "-".join(map(str, key_parts))
+    def getCacheKey(self, func_name: str, *args, **kwargs) -> str:
+        """生成稳定且唯一的缓存键"""
+        args_str = "-".join(map(str, args))
+        # 对kwargs的键排序以确保顺序一致
+        kwargs_str = "-".join(f"{k}={v}" for k, v in sorted(kwargs.items()))
+        key = f"{func_name}-{args_str}-{kwargs_str}"
+        # 使用哈希进一步压缩键（可选）
+        return hashlib.md5(key.encode()).hexdigest()
 
-    def getData(self, func_name: str, *args, **kwargs):
-        """从缓存中获取数据,如果缓存不存在或过期则返回None"""
+    def getData(self, func_name: str, *args, **kwargs) -> any:
         cache_key = self.getCacheKey(func_name, *args, **kwargs)
         return self.cache.get(cache_key)
 
-    def setData(self, func_name: str, data, *args, **kwargs):
-        """设置缓存数据"""
+    def setData(self, func_name: str, data: any, *args, **kwargs):
         cache_key = self.getCacheKey(func_name, *args, **kwargs)
         self.cache.set(cache_key, data)
